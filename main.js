634bfe87f9782fba68f9ff38905654103440ab6d
Michal Koptewicz <mkoptewicz@gmail.com>
2020-12-17 15:04:02 +0100

    add animation to services

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 const nav = document.querySelector(".nav-container");
 const header = document.querySelector("header");
+const sliders = document.querySelectorAll(".slide-in");
 const headerOptions = {
- rootMargin:"-100% 0px 0px 0px"
+  rootMargin: "-100% 0px 0px 0px",
 };
 const headerObserver = new IntersectionObserver(function (
   entries,
   headerObserver
 ) {
   entries.forEach(entry => {
-    if(!entry.isIntersecting){
-     nav.classList.add('nav-container--scrolled')
+    if (!entry.isIntersecting) {
+      nav.classList.add("nav-container--scrolled");
     } else {
-     nav.classList.remove('nav-container--scrolled')
+      nav.classList.remove("nav-container--scrolled");
     }
   });
 },
 headerOptions);
 headerObserver.observe(header);
+const sliderOptions = {
+  threshold: 0.3,
+};
+
+const showOnScroll = new IntersectionObserver(function (entries) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    else {
+      entry.target.classList.add("active");
+      showOnScroll.unobserve(entry.target);
+    }
+  });
+}, sliderOptions);
+sliders.forEach(slider => {
+  showOnScroll.observe(slider);
+});
